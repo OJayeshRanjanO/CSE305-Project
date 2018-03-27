87e82effdef252d866212e994ada924d1a74ce05
OJayeshRanjanO <jayesh.ranjan20@gmail.com>
2018-03-27 11:11:17 -0400

    Test INSERT query added

--- dbFunctions.py
+++ dbFunctions.py
@@ -2,4 +2,14 @@ from dbConnect import connect_db
 
 
 def register_user():
-    return True
+    #This is just test CODE
+    connection = connect_db()
+    query = "INSERT into TEST (A,B) values (2, 2)"
+    cursor = connection.cursor()
+    cursor.execute(query)
+    connection.commit()
+    connection.close()
+
+
+if __name__ == '__main__':
+    register_user()
