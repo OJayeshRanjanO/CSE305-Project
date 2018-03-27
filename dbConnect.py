cfe0642eecb61cadc4968584928b60baa46c8c9a
Feazan <feazan.yaseen@stonybrook.edu>
2018-03-27 10:21:33 -0400

    Test

--- dbConnect.py
+++ dbConnect.py
@@ -1,9 +1,9 @@
-import pymysql.cursors
+# import pymysql.cursors
 
 
 def connect_db():
-    return pymysql.connect(host='cse305.ctn44dwr3swf.us-east-2.rds.amazonaws.com',
-                    user='cse305Team1',
-                    passwd='cse305Team1',
-                    db='cse305',
-                    cursorclass=pymysql.cursors.DictCursor)
+    return False#pymysql.connect(host='cse305.ctn44dwr3swf.us-east-2.rds.amazonaws.com',
+                    #user='cse305Team1',
+                    #passwd='cse305Team1',
+                    #db='cse305',
+                    #cursorclass=pymysql.cursors.DictCursor)
