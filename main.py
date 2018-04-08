65ea3753955b856c28c666369269d93220da61fe
OJayeshRanjanO <jayesh.ranjan20@gmail.com>
2018-04-08 01:07:48 -0400

    Session supported added

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, session
 from dbFunctions import *
 import json
 from loginPageFunctions import *
 
 app = Flask(__name__)
+app.secret_key = "TEST"
 
 @app.route('/')
 # @app.route("/<user>")
@@ -15,13 +16,15 @@ def checkLogin():
     recvJson = request.get_json()
     email = recvJson['email']
     password = recvJson['pwd']
+    session['email'] = email
     returnValue = checkPassengerCredentials(email,password)
     return str(json.dumps({"login":"true"})) if returnValue else str(json.dumps({"login":"false"}))
 
 
 @app.route('/home')
 def home():
-    return render_template("profile.html")
+    print(session['email'])
+    return render_template("home.html")
 
 
 if __name__ == '__main__':
