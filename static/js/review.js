0f9d0e2531539942b118035f507db54062b5c2c4
Lucy Yang <lucyy@nyclabschool.org>
2018-04-26 19:33:22 -0400

    linking review to other pages

---
+++ static/js/review.js
@@ -0,0 +1,8 @@
+function redirectTo(id){
+  // alert(id);
+  if (id === "cruise"){window.location.href = "/cruises"};
+  if (id === "flight"){window.location.href = "/home"};
+  if (id === "hotel"){window.location.href = "/hotels"};
+  if (id === "car"){window.location.href = "/cars"};
+
+}
