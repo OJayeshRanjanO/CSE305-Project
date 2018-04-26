b10399847f53121dc9bffa6ca3d4b00ce909b9aa
fyaseen <feazany@gmail.com>
2018-04-26 13:55:32 -0400

    Register with empty query works

--- static/js/registerPage.js
+++ static/js/registerPage.js
@@ -1,38 +1,51 @@
-function register()
+function validate_form()
 {
-    if (validate_form() === true)
+    var nameLength = $("#name").val().length;
+    var ageLength = $("#age").val().length;
+    var emailLength = $("#email").val().length;
+    var passwordLength = $("#pwd").val().length;
+
+    if (nameLength > 0 && ageLength > 0 && emailLength > 0 && passwordLength > 0)
     {
-        // This parses all the data from the form
-        var registerData = $('#registerForm').serialize();
-        var gender = $('#gender').val();
-        registerData += "&gender=" + gender;
-        // Stores it to a JSON type
-        registerData = toJSON(registerData);
-        $.ajax({
-            type: "POST",
-            url: "/registerUser",
-            data: registerData,
-            dataType: "json",
-            contentType : "application/json"
-        }).done(function (data, textStatus, jqXHR)
-        {
-            // data.login from main.py
-            data = $.parseJSON(JSON.stringify(data));
-            if (data.registered === "fail")
-            {
-                $("#registerContainer").append("<div style='color:red'> Incorrect info </div>")
-            }
-            else if (data.registered === "true")
-            {
-                // localStorage.setItem("username", data.username);
-                window.location.href = "/home"
-            }
-        });
+        register();
+        return true;
     }
     else
     {
-        alert("Please fill out all fields");
+        //alert("Please fill out form");
+        return false;
     }
+}
+
+function register()
+{
+    // This parses all the data from the form
+    var registerData = $('#registerForm').serialize();
+    var gender = $('#gender').val();
+    registerData += "&gender=" + gender;
+    // Stores it to a JSON type
+    registerData = toJSON(registerData);
+    $.ajax({
+        type: "POST",
+        url: "/registerUser",
+        data: registerData,
+        dataType: "json",
+        contentType : "application/json"
+    }).done(function (data, textStatus, jqXHR)
+    {
+        // data.login from main.py
+        data = $.parseJSON(JSON.stringify(data));
+        if (data.registered === "fail")
+        {
+            $("#registerContainer").append("<div style='color:red'> Incorrect info </div>")
+        }
+        else if (data.registered === "true")
+        {
+            // localStorage.setItem("username", data.username);
+            window.location.href = "/home"
+        }
+    });
+
 }
 
 function toJSON(data)
@@ -47,20 +60,3 @@ function toJSON(data)
   // obj['username'] = localStorage['username']
   return JSON.stringify(obj);
 }
-
-function validate_form()
-{
-    var nameLength = $("#name").size();
-    var ageLength = $("#age").size();
-    var emailLength = $("#email").size();
-    var passwordLength = $("#pwd").size();
-
-    console.log("Length of name: " + nameLength);
-    console.log("Length of age: " + ageLength);
-    console.log("Length of email: " + emailLength);
-    if (nameLength > 0 && ageLength > 0 && emailLength > 0 && passwordLength > 0)
-        return true;
-    else
-        return false;
-}
-
