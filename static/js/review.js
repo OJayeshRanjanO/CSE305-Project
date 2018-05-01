c15fbd49ec687d7edc9960537042c68908bf5016
OJayeshRanjanO <jayesh.ranjan20@gmail.com>
2018-05-01 09:29:02 -0400

    Modified resourceBanners

--- static/js/review.js
+++
@@ -1,8 +0,0 @@
-function redirectTo(id){
-  // alert(id);
-  if (id === "cruise"){window.location.href = "/cruises"};
-  if (id === "flight"){window.location.href = "/home"};
-  if (id === "hotel"){window.location.href = "/hotels"};
-  if (id === "car"){window.location.href = "/cars"};
-  if (id === "review"){window.location.href = "/review"};
-}
